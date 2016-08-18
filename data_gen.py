271cc1d7c3191b8c812e4ff841777a0a6dac2f6a
Chase Williams <bornofawesomeness@gmail.com>
2016-08-18 17:27:59 -0400

    successfully gather genre data

--- data_gen.py
+++ data_gen.py
@@ -3,7 +3,7 @@ import requests as http
 import pprint
 
 library_data = {}
-token = 'BQDTWMPn1EMRk-u2DN74mEK-X-BxGTM3oqxRTFgsHyGRiCt3Z3ie_ilygpXK0RULAt8iN1iOgslhBFW4hlrwI_Pxy-kRX1XMmNKXF63Y63jZfSnPLuRhm55qT3NnOcSfp38kIxroIs5A4lBkkCnCB0EawFtdZ5MB'
+token = 'BQBOCBoZ9vzHbQl01hfc_eOV9zFRqyY90ge1mSYYHU_BIrLuSIVFXLJ7LpjmR_m2F3KZdvP8Xp4ovmOEuSgD3q0q2elxG__PkoRzrGUm0nppn68Fn9e-yGC6pa8LSH91lXlzME5Lz5n-OwTd-hHew4kUXOWGbI5m'
 api_base = 'https://api.spotify.com/v1'
 api_library = api_base + '/me/tracks'
 api_artists = api_base + '/artists'
@@ -21,9 +21,11 @@ def determine_genres(ids):
     string = ''
     genres = [] # each elem's index corresponds with ids' index
     for id in ids:
-        string += id
-    response = http.get(api_artists + '?ids=' + string, headers={'Authorization': 'Bearer ' + token})
-    json = response.json()
+        string += id + ','
+    string = string.rstrip(',')
+    print("\n\n\n" + string)
+    artists_get_result = http.get(api_artists + '?ids=' + string, headers={'Authorization': 'Bearer ' + token})
+    json = artists_get_result.json()
     for artist in json['artists']:
         genres.append(artist['genres'])
     return genres
@@ -49,14 +51,45 @@ for offset in list(range(1, total_songs, 50)):
 ## Figure out what genre the artists are from
 
 keys = library_data.keys()
-for index in list(range(1, total_songs, 50)):
-    sub_arr = keys[:index]
-    string = ''
+keys = list(keys)
+artists_genres = []
+total_artists = len(library_data)
+range_ = range(1, total_artists, 50)
+print(list(range_))
+for index in list(range_):
+    if index + 50 > total_artists:
+        final_index = total_artists
+    else:
+        final_index = index + 50
+
+    sub_arr = keys[index:final_index]
+    id_arr = []
     for key in sub_arr:
-        string += library_data[key]['id']
-    string.index(0, str)
+        id_arr.append(library_data[key]['id'])
+    artists_genres.append( determine_genres(id_arr) )
+
+## Analyze artist_genres for genre counts
+
+genre_count = {}
+for specific_artist in artists_genres:
+    for genres in specific_artist:
+        for genre in genres:
+            print(type(genre))
+            if genre not in genre_count:
+                genre_count[genre] = 1
+            else:
+                genre_count[genre] += 1
+
 
 
 ## visualize result
+
 pprint.pprint(library_data)
+pprint.pprint(genre_count)
 print(len(library_data))
+print(total_artists)
+genre_keys = genre_count.keys()
+count = 0
+for key in genre_keys:
+    count += genre_count[key]['count']
+print(count)
